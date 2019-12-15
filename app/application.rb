773b9af1cf31d20aaa0deab4d3c991818a8b3dc2
Olivia Kesler <oakesler@protonmail.ch>
2019-12-15 05:15:52 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,28 @@
+class Item
+  attr_accessor :name, :price
+  
+  @@items = []
+
+  def initialize(name,price)
+    @name = name
+    @price = price
+  
+  #def call(env)
+    #resp = Rack::Response.new
+    #req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = Item.find{|s| s.name == item_name}
+      if !item
+        resp.write "Item not found"
+        resp.status 400
+      else
+        resp.write item.price
+      end 
+    else 
+      resp.write "Error!"
+      resp.status 404
+    end
+  end
+end
