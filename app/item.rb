77e06d945f1383aead2ee31181fda9a98b1ee12b
Olivia Kesler <oakesler@protonmail.ch>
2019-12-15 05:24:23 +0000

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,27 +1,31 @@
 class Item
   attr_accessor :name, :price
+  
+  @@items = []
 
   def initialize(name,price)
     @name = name
     @price = price
-  end
-  
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-    
-    if req.path.match(/items/)
-      item_name = req.path.split("/items/").last
-      item = Item.find{|s| s.name == item_name}
-      if !item
-        resp.write "Item not found"
-        resp.status 400
-      else
-        resp.write item.price
-      end 
-    else 
-      resp.write "Error!"
-      resp.status 404
-    end
+    @@items << self
   end
 end
+  
+  #def call(env)
+    #resp = Rack::Response.new
+    #req = Rack::Request.new(env)
+    
+    #if req.path.match(/items/)
+      #item_name = req.path.split("/items/").last
+      #item = Item.find{|s| s.name == item_name}
+      #if !item
+        #resp.write "Item not found"
+        #resp.status 400
+      #else
+        #resp.write item.price
+      #end 
+    #else 
+      #resp.write "Error!"
+      #resp.status 404
+  #  end
+  #end
+#end
