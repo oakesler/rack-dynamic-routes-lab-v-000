9eeb7eeb7af369cf8984964da13164249b3bb9bb
Olivia Kesler <oakesler@protonmail.ch>
2019-12-15 05:16:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,10 @@
 class Application
   
   @@items = []
-
-  def initialize(name,price)
-    @name = name
-    @price = price
   
-  #def call(env)
-    #resp = Rack::Response.new
-    #req = Rack::Request.new(env)
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
