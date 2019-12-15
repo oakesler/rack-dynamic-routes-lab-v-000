3a43e8eff2a8ca9802da8ee3685796af9099ff97
Olivia Kesler <oakesler@protonmail.ch>
2019-12-15 05:11:43 +0000

    Done.

--- app/item.rb
+++ app/item.rb
@@ -4,42 +4,24 @@ class Item
   def initialize(name,price)
     @name = name
     @price = price
-    
-  resp = Rack::Response.new
-  req = Rack::Request.new(env)
- 
-  if req.path.match(/items/)
-    item_name = req.path.split("/items/").last #turn 
-    item = Item.find{|s| s.name == item_name}
-    #item_price = item.price
-    resp.write item.price
   end
- 
- 
- 
- 
-  if req.path=="/items"
-     resp.write "You requested the songs"
-    else
-      resp.write "Route not found"
-      resp.status = 404
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = Item.find{|s| s.name == item_name}
+      if !item
+        resp.write "Item not found"
+        resp.status 400
+      else
+        resp.write item.price
+      end 
+    else 
+      resp.write "Error!"
+      resp.status 404
     end
- 
-    resp.finish
   end
 end
-
-
- if req.path.match(/songs/)
- 
-      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-      song = @@songs.find{|s| s.title == song_title}
- 
-      resp.write song.artist
-    end
-
-
-Your application should only accept the /items/<ITEM NAME> route. Everything else should 404
-
-If a user requests /items/<Item Name> it should return the price of that item
-IF a user requests an item that you don't have, then return a 400 and an error message
