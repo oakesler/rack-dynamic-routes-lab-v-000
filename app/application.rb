cbb696a924f3e46e6473873fa82ca26c5a7de4d9
Olivia Kesler <oakesler@protonmail.ch>
2019-12-15 05:19:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,9 @@ class Application
   
   #@@items = []
   
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+  #def call(env)
+    #resp = Rack::Response.new
+    #req = Rack::Request.new(env)
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
