b32937aecc450643aaf3198318bda2c45511a2e4
Olivia Kesler <oakesler@protonmail.ch>
2019-12-15 05:17:08 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -11,18 +11,18 @@ class Item
     #resp = Rack::Response.new
     #req = Rack::Request.new(env)
     
-    if req.path.match(/items/)
-      item_name = req.path.split("/items/").last
-      item = Item.find{|s| s.name == item_name}
-      if !item
-        resp.write "Item not found"
-        resp.status 400
-      else
-        resp.write item.price
-      end 
-    else 
-      resp.write "Error!"
-      resp.status 404
-    end
-  end
-end
+    #if req.path.match(/items/)
+      #item_name = req.path.split("/items/").last
+      #item = Item.find{|s| s.name == item_name}
+      #if !item
+        #resp.write "Item not found"
+        #resp.status 400
+      #else
+        #resp.write item.price
+      #end 
+    #else 
+      #resp.write "Error!"
+      #resp.status 404
+  #  end
+  #end
+#end
