40593015af920b962bef28969ddbcfb93d45da84
Olivia Kesler <oakesler@protonmail.ch>
2019-12-15 05:12:49 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -5,9 +5,9 @@ class Item
     @name = name
     @price = price
   
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+  #def call(env)
+    #resp = Rack::Response.new
+    #req = Rack::Request.new(env)
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
